a47aecbad9f3c8ce68e864f58f3e4fe92397a429
TheivaGit <theiva2791@gmail.com>
2025-09-02 12:25:38 +0530

    mock data rendering

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import ReactDOM from "react-dom/client";
 import HeaderComp from "./components/header-comp";
 import BodyComp from "./components/Body-comp";
 
-
 const AppLayout = () => {
-    return (
-        <div className="appLayout">
-            <HeaderComp />
-            <BodyComp />
-        </div>
-    )
-}
+  return (
+    <div className="appLayout">
+      <HeaderComp />
+      <BodyComp />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<AppLayout />); // replace any content inside the root
--- src/components/Body-comp.js
+++ src/components/Body-comp.js
@@ -1,41 +1,64 @@
 import restoData from "../utils/resto-card-data";
 import RestoCardComp from "./Resto-card-comp";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const BodyComp = () => {
+  // local state variable
+  const [restoDataList, setRestoDataList] = useState(restoData);
+  let restData;
 
-    // local state variable
-    const [restoDataList, setrestoDataList] = useState(restoData);
+  useEffect(() => {
+    fetchResto();
+  }, []);
+ 
+  const fetchResto = async () => {
+    const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+    console.log('data', data);
+    const resJson = await data.json();
 
-    console.log("restoDataList", restoDataList);
+     restData = resJson?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
 
-    return (
-        <div className="body">
-            <div className="searchBar">
-                <button className="searchBtn" onClick={()=>{
-                    console.log("btn-clicked");
-                   const filteredArr =  restoDataList.filter((elm)=>elm.info.avgRating > 4);
-                   setrestoDataList(filteredArr); //update the list => in normal function we do list.push or etc here we we do with hooks(noraml js function given by react developers)
-                }}>
-                         
-                    Top Restaurents 
-                </button>
-                <button className="searchBtn" onClick={()=>{
-                    console.log("btn-clicked");
-                   const filteredArr =  restoData;
-                   setrestoDataList(filteredArr); //update the list => in normal function we do list.push or etc here we we do with hooks(noraml js function given by react developers)
-                }}>
-                         
-                  Clear filter
-                </button>
-            </div>
-            <div className="restContainer">
-               {restoDataList.map(resturent => (
-               <RestoCardComp key={resturent.info.id} resturantData={resturent} />
-               ))}
-            </div>
-        </div>
-    );
+    
+    setRestoDataList(restData);
+
+    console.log('resJson', restData);
+  }
+
+  console.log("restoDataList", restoDataList, restData);
+
+  return (
+    <div className="body">
+      <div className="searchBar">
+        <button
+          className="searchBtn"
+          onClick={() => {
+            console.log("btn-clicked");
+            const filteredArr = restoDataList.filter(
+              (elm) => elm.info.avgRating > 4.2
+            );
+            setRestoDataList(filteredArr); //update the list => in normal function we do list.push or etc here we we do with hooks(noraml js function given by react developers)
+          }}
+        >
+          Top Restaurents
+        </button>
+        <button
+          className="searchBtn"
+          onClick={() => {
+            console.log("btn-clicked");
+            const filteredArr = restoDataList;
+            setRestoDataList(restoDataList); //update the list => in normal function we do list.push or etc here we we do with hooks(noraml js function given by react developers)
+          }}
+        >
+          Clear filter
+        </button>
+      </div>
+      <div className="restContainer">
+        {restoDataList.map((resturent) => (
+          <RestoCardComp key={resturent?.info?.id} resturantData={resturent} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default BodyComp;
